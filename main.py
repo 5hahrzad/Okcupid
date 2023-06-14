db9629556f6913b9bc362e7cf3a432ad763aeddf
5hahrzad <sakin.ghaderi@gmail.com>
2023-06-14 13:16:33 +0200

    update

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# The code is importing three Python libraries: pandas, numpy, and plotly.express. The `as` keyword is
+# used to create an alias for each library, which allows for easier referencing of the library's
+# functions and methods later in the code.
 ## importing libraries
 import pandas as pd
 import numpy as np
